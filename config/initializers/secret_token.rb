0346ffad0705a577fa3880d34c86915df5f4a620
Santisuk <peacesantisuk@gmail.com>
2013-09-13 20:54:36 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IseLounge::Application.config.secret_token = 'd149ad192cf9d96b70791453a1a0f5737d2cc94001457b4aef5950c51af06a6cf5d056904dbd23cc365b26624636ad7a46136488b715cce003d07ebb57b8065a'
